bfe66172cc07f104bc07e476fe7008c322e9a6e6
Cut <web.cutfox@gmail.com>
2025-08-01 12:26:50 +0900

    asdasd

--- bot/server.js
+++ bot/server.js
@@ -78,7 +78,7 @@ app.post('/lava-webhook', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🔄 Webhook server listening on port ${PORT}`);
 });
